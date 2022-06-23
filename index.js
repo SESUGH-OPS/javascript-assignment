3991b57020c355ae2ccb64b264bdbe85a6211fd8
sesugh97 <105423735+sesugh97@users.noreply.github.com>
2022-06-23 11:51:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let myName ="SIMEON SESUGH AMIASE";
+let myHeight="5'7 FEET";
+let myCountry="NIGERIA";
+
+
+document.write("Name :"+ myName + "<br>" + "Height :" + myHeight + "<br>" + "Country :"+ myCountry)
